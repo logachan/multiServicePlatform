9a75870c565053341e33ce5d1cc00cfa526d050f
logachan <logachan08@gmail.com>
2025-09-09 16:22:37 +0530

    removed dist

--- backend/dist/api/bulk-email/dto/create-bulk-email.dto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CreateBulkEmailDto = void 0;
-class CreateBulkEmailDto {
-}
-exports.CreateBulkEmailDto = CreateBulkEmailDto;
-//# sourceMappingURL=create-bulk-email.dto.js.map
--- backend/dist/api/bulk-email/entities/bulk-email.entity.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BulkEmail = void 0;
-class BulkEmail {
-}
-exports.BulkEmail = BulkEmail;
-//# sourceMappingURL=bulk-email.entity.js.map
